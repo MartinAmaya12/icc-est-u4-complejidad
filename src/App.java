3af7ba7493b0d8d4f2904a7955569c13fd33032c
Martin Amaya <ramayst@est.ups.edu.ec>
2026-04-15 14:59:11 -0500

    UPD : avance

--- src/App.java
+++ src/App.java
@@ -1,18 +1,16 @@
+
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int cantidadEstudiantes = 30000000;
+        int cantidad = 1_000_000;   //formas de represntar cantidades en java
+        
+        List<Estudiante> estudiantes = Generador.generarListado(cantidadEstudiantes);
 
-        int[] array = new int[]{1,2,3,4,5};
-        System.out.println(array[0]);
-
-        //FOR normal
-        for(int i = 0; i < array.length; i++){
-            System.out.print(array[i] + " ");
+        for(Estudiante est: estudiantes){
+            System.out.println(est.nombre);
         }
 
-        //FOR EACH
-        for(int i : array){
-            System.out.println(i);
-        }
     }
 }
